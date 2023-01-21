d229a61a1534b94d2803d65437fc6cb01d6be745
AndrewPerepelkin <avp.tep@rambler.ru>
2023-01-21 12:47:35 +0300

    feat: added badges colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    safelist: ["bg-green-600", "bg-teal-500", "bg-orange-600"],
     theme: {
         extend: {}
     },
